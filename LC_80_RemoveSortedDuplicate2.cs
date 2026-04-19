fa209beae02c3acbae54c37e9a803415ae9d7b80
VikramPunia125 <85619413+VikramPunia125@users.noreply.github.com>
2026-04-20 00:51:38 +0530

    Genericn sol allows D Duplicates

--- LC_80_RemoveSortedDuplicate2.cs
+++ LC_80_RemoveSortedDuplicate2.cs
@@ -2,15 +2,16 @@ namespace Solution
 {
     public class Solution
     {
+        static int d = 2;
         public static int RemoveDuplicates(int[] nums)
         {
-            if (nums.Length <= 2)
+            if (nums.Length <= d)
                 return nums.Length;
 
-            int k = 2;
-            for (int i = 2; i < nums.Length; i++)
+            int k = d;
+            for (int i = d; i < nums.Length; i++)
             {
-                if (nums[i] != nums[k - 2])
+                if (nums[i] != nums[k - d])
                 {
                     nums[k] = nums[i];
                     k++;
@@ -23,9 +24,11 @@ namespace Solution
     {
         static void Main(string[] args)
         {
-            int[] nums = new int[] { 1, 1, 1, 2, 2, 3 };
-            int result = Solution.RemoveDuplicates(nums);
+            int[] nums = new int[] { 1, 1, 1, 1, 2, 2, 3 };
             Console.WriteLine(string.Join(", ", nums));
+            Console.WriteLine(nums.Length);
+            int result = Solution.RemoveDuplicates(nums);
+            Console.WriteLine(string.Join(", ", nums.Take(result)));
             Console.WriteLine(result);
         }
     }

7f8e3cb653f1430ad981b6424dfa82521719e6c1
VikramPunia125 <85619413+VikramPunia125@users.noreply.github.com>
2026-04-20 02:27:14 +0530

    Juddgling Method

--- LC_189_RotateKIndex.cs
+++ LC_189_RotateKIndex.cs
@@ -1,32 +1,24 @@
-public class Solution
-{
-    public void Rotate(int[] nums, int k)
-    {
-        if (nums == null || nums.Length <= 1)
-        {
-            return;
+public class Solution {
+    public void Rotate(int[] nums, int k) {
+        k=k%nums.Length;
+        if(k==0)return; 
+        int hcf= HCF(nums.Length,k);
+        System.Console.WriteLine(hcf);
+        for(int i=0;i<hcf;i++){
+            int s=i+k, n=nums[i];
+            do{
+                int d =nums[s];
+                nums[s]=n;
+                n=d;
+                s=(s+ k) % nums.Length;
+            }while(s!=i);
+            nums[s]=n;
         }
-
-        k %= nums.Length;
-        if (k == 0)
-        {
-            return;
-        }
-
-        Reverse(nums, 0, nums.Length - 1);
-        Reverse(nums, 0, k - 1);
-        Reverse(nums, k, nums.Length - 1);
     }
-
-    private void Reverse(int[] nums, int left, int right)
-    {
-        while (left < right)
-        {
-            int temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-            left++;
-            right--;
-        }
+    public static int HCF(int n1, int n2){
+        if(n1<n2)
+        return HCF(n2,n1);
+        if(n1%n2==0) return n2;
+        return HCF(n2,(n1%n2));
     }
 }
